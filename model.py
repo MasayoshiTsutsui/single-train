99060805c763ba23a860c3a09fcd8f6fd7a6a69f
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2023-02-26 23:58:50 +0900

    a

--- model.py
+++ model.py
@@ -3,267 +3,98 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class KaimingConv(nn.Conv2d):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class BasicBlock(nn.Module):
+    expansion = 1
 
-        # NOTE: initialize the weights like this.
-        nn.init.kaiming_normal_(self.weight, mode="fan_in", nonlinearity="relu")
-        nn.Parameter(self.weight)
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-    def forward(self, x):
-        x = F.conv2d(
-            x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups
-        )
-        return x
-
-class KaimingLinear(nn.Linear):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
-        # NOTE: initialize the weights like this.
-        nn.init.kaiming_normal_(self.weight, mode="fan_in", nonlinearity="relu")
-        nn.Parameter(self.weight)
-        nn.Parameter(self.bias)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
 
     def forward(self, x):
-        return F.linear(x, self.weight, self.bias)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-# NOTE: We use non affine batch norm
-class NonAffineBatchNorm(nn.BatchNorm2d):
-    def __init__(self, dim):
-        super(NonAffineBatchNorm, self).__init__(dim, affine=False)
 
-class HasAffineBatchNorm(nn.BatchNorm2d):
-    def __init__(self, dim):
-        super(HasAffineBatchNorm, self).__init__(dim, affine=True)
+class Bottleneck(nn.Module):
+    expansion = 4
 
-class Conv6(nn.Module):
-    def __init__(self):
-        super(Conv6, self).__init__()
-        self.conv1 = KaimingConv(3, 64, 3, stride=1, padding=1, bias=False)
-        self.bn1 = HasAffineBatchNorm(64)
-        self.conv2 = KaimingConv(64, 64, 3, stride=1, padding=1, bias=False)
-        self.bn2 = HasAffineBatchNorm(64)
-        self.conv3 = KaimingConv(64, 128, 3, stride=1, padding=1, bias=False)
-        self.bn3 = HasAffineBatchNorm(128)
-        self.conv4 = KaimingConv(128, 128, 3, stride=1, padding=1, bias=False)
-        self.bn4 = HasAffineBatchNorm(128)
-        self.conv5 = KaimingConv(128, 256, 3, stride=1, padding=1, bias=False)
-        self.bn5 = HasAffineBatchNorm(256)
-        self.conv6 = KaimingConv(256, 256, 3, stride=1, padding=1, bias=False)
-        self.bn6 = HasAffineBatchNorm(256)
-        self.fc1 = KaimingLinear(4*4*256, 256, bias=False)
-        self.bn7 = nn.BatchNorm1d(num_features=256, affine=False)
-        self.fc2 = KaimingLinear(256, 256, bias=False)
-        self.bn8 = nn.BatchNorm1d(num_features=256, affine=False)
-        self.fc3 = KaimingLinear(256, 10, bias=False)
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = F.relu(x)
-        x = self.conv4(x)
-        x = self.bn4(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.conv5(x)
-        x = self.bn5(x)
-        x = F.relu(x)
-        x = self.conv6(x)
-        x = self.bn6(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.bn7(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = self.bn8(x)
-        x = F.relu(x)
-        output = self.fc3(x)
-        #output = F.log_softmax(x, dim=1)
-        return output
-
-class ResNet18(nn.Module):
-    channels1 = 32
-    channels2 = 64
-    channels3 = 128
-    channels4 = 256
-    def __init__(self):
-        super(ResNet18, self).__init__()
-        #layer1
-        self.conv1 = KaimingConv(3, self.channels1, kernel_size=3, stride=1, padding=1, bias=False) 
-        self.bn1 = HasAffineBatchNorm(self.channels1)
-
-        ##layer2
-        self.conv2_1 = KaimingConv(self.channels1, self.channels1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2_1 = HasAffineBatchNorm(self.channels1)
-        self.conv2_2 = KaimingConv(self.channels1, self.channels1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2_2 = HasAffineBatchNorm(self.channels1)
-        self.conv2_3 = KaimingConv(self.channels1, self.channels1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2_3 = HasAffineBatchNorm(self.channels1)
-        self.conv2_4 = KaimingConv(self.channels1, self.channels1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2_4 = HasAffineBatchNorm(self.channels1)
-
-        ##layer3
-        self.downsample3_1 = KaimingConv(self.channels1, self.channels2, kernel_size=1, stride=2, padding=0, bias=False) #residualのchannel数とfeature map sizeをそろえる
-        self.dbn3_1 = HasAffineBatchNorm(self.channels2)
-
-        self.conv3_1 = KaimingConv(self.channels1, self.channels2, kernel_size=3, stride=2, padding=1, bias=False) #ここでfeature mapサイズは半減
-        self.bn3_1 = HasAffineBatchNorm(self.channels2)
-        self.conv3_2 = KaimingConv(self.channels2, self.channels2, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn3_2 = HasAffineBatchNorm(self.channels2)
-        self.conv3_3 = KaimingConv(self.channels2, self.channels2, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn3_3 = HasAffineBatchNorm(self.channels2)
-        self.conv3_4 = KaimingConv(self.channels2, self.channels2, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn3_4 = HasAffineBatchNorm(self.channels2)
-
-        ##layer4
-        self.downsample4_1 = KaimingConv(self.channels2, self.channels3, kernel_size=1, stride=2, padding=0, bias=False) #residualのchannel数とfeature map sizeをそろえる
-        self.dbn4_1 = HasAffineBatchNorm(self.channels3)
-
-        self.conv4_1 = KaimingConv(self.channels2, self.channels3, kernel_size=3, stride=2, padding=1, bias=False) #ここでfeature mapサイズは半減
-        self.bn4_1 = HasAffineBatchNorm(self.channels3)
-        self.conv4_2 = KaimingConv(self.channels3, self.channels3, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn4_2 = HasAffineBatchNorm(self.channels3)
-        self.conv4_3 = KaimingConv(self.channels3, self.channels3, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn4_3 = HasAffineBatchNorm(self.channels3)
-        self.conv4_4 = KaimingConv(self.channels3, self.channels3, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn4_4 = HasAffineBatchNorm(self.channels3)
-
-        ##layer5
-        self.downsample5_1 = KaimingConv(self.channels3, self.channels4, kernel_size=1, stride=2, padding=0, bias=False) #residualのchannel数とfeature map sizeをそろえる
-        self.dbn5_1 = HasAffineBatchNorm(self.channels4)
-
-        self.conv5_1 = KaimingConv(self.channels3, self.channels4, kernel_size=3, stride=2, padding=1, bias=False) #ここでfeature mapサイズは半減
-        self.bn5_1 = HasAffineBatchNorm(self.channels4)
-        self.conv5_2 = KaimingConv(self.channels4, self.channels4, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn5_2 = HasAffineBatchNorm(self.channels4)
-        self.conv5_3 = KaimingConv(self.channels4, self.channels4, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn5_3 = HasAffineBatchNorm(self.channels4)
-        self.conv5_4 = KaimingConv(self.channels4, self.channels4, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn5_4 = HasAffineBatchNorm(self.channels4)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
 
-        ##last layer
-        self.avgpool = nn.AvgPool2d(4) #feature mapを1x1サイズになるように平均化
-        self.fc = KaimingLinear(self.channels4, 10, bias=True)
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
 
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
 
     def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
 
-        ##layer1
-        out1 = self.conv1(x)
-        out1 = self.bn1(out1)
-        out1 = F.relu(out1, inplace=True)
 
-        ##layer2
-        residual2_1 = out1
-
-        out2_1 = self.conv2_1(out1)
-        out2_1 = self.bn2_1(out2_1)
-        out2_1 = F.relu(out2_1, inplace=True)
-
-        out2_1 = self.conv2_2(out2_1)
-        out2_1 = self.bn2_2(out2_1)
-        out2_1 += residual2_1
-        out2_1 = F.relu(out2_1, inplace=True)
-
-        residual2_2 = out2_1
-
-        out2_2 = self.conv2_3(out2_1)
-        out2_2 = self.bn2_3(out2_2)
-        out2_2 = F.relu(out2_2, inplace=True)
-
-        out2_2 = self.conv2_4(out2_2)
-        out2_2 = self.bn2_4(out2_2)
-        out2_2 += residual2_2
-        out2_2 = F.relu(out2_2, inplace=True)
-
-        ##layer3
-        residual3_1 = self.downsample3_1(out2_2)
-        residual3_1 = self.dbn3_1(residual3_1)
-
-        out3_1 = self.conv3_1(out2_2)
-        out3_1 = self.bn3_1(out3_1)
-        out3_1 = F.relu(out3_1, inplace=True)
-
-        out3_1 = self.conv3_2(out3_1)
-        out3_1 = self.bn3_2(out3_1)
-        out3_1 += residual3_1
-        out3_1 = F.relu(out3_1, inplace=True)
-
-        residual3_2 = out3_1
-
-        out3_2 = self.conv3_3(out3_1)
-        out3_2 = self.bn3_3(out3_2)
-        out3_2 = F.relu(out3_2, inplace=True)
-
-        out3_2 = self.conv3_4(out3_2)
-        out3_2 = self.bn3_4(out3_2)
-        out3_2 += residual3_2
-        out3_2 = F.relu(out3_2, inplace=True)
-
-        ##layer4
-        residual4_1 = self.downsample4_1(out3_2)
-        residual4_1 = self.dbn4_1(residual4_1)
-
-        out4_1 = self.conv4_1(out3_2)
-        out4_1 = self.bn4_1(out4_1)
-        out4_1 = F.relu(out4_1, inplace=True)
-
-        out4_1 = self.conv4_2(out4_1)
-        out4_1 = self.bn4_2(out4_1)
-        out4_1 += residual4_1
-        out4_1 = F.relu(out4_1, inplace=True)
-
-        residual4_2 = out4_1
-
-        out4_2 = self.conv4_3(out4_1)
-        out4_2 = self.bn4_3(out4_2)
-        out4_2 = F.relu(out4_2, inplace=True)
-
-        out4_2 = self.conv4_4(out4_2)
-        out4_2 = self.bn4_4(out4_2)
-        out4_2 += residual4_2
-        out4_2 = F.relu(out4_2, inplace=True)
-
-        ##layer4
-        residual5_1 = self.downsample5_1(out4_2)
-        residual5_1 = self.dbn5_1(residual5_1)
-
-        out5_1 = self.conv5_1(out4_2)
-        out5_1 = self.bn5_1(out5_1)
-        out5_1 = F.relu(out5_1, inplace=True)
-
-        out5_1 = self.conv5_2(out5_1)
-        out5_1 = self.bn5_2(out5_1)
-        out5_1 += residual5_1
-        out5_1 = F.relu(out5_1, inplace=True)
-
-        residual5_2 = out5_1
-
-        out5_2 = self.conv5_3(out5_1)
-        out5_2 = self.bn5_3(out5_2)
-        out5_2 = F.relu(out5_2, inplace=True)
-
-        out5_2 = self.conv5_4(out5_2)
-        out5_2 = self.bn5_4(out5_2)
-        out5_2 += residual5_2
-        out5_2 = F.relu(out5_2, inplace=True)
-
-        ##last layer
-        output = self.avgpool(out5_2)
-        output = torch.flatten(output, 1)
-        output = self.fc(output)
-
-        return output
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
--- train.py
+++ train.py
@@ -10,9 +10,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.optim.lr_scheduler import ExponentialLR
-from torchvision import datasets, transforms
-from model import Conv6
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torchvision import datasets, transforms, models
+from model import ResNet18
 DEBUG = 1
 
 def dbg_print(s):
@@ -23,8 +23,10 @@ def dbg_print(s):
 
 from collections import OrderedDict
 
-def train(model, device, train_loader, criterion, epochs):
-    optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad])
+def train(model, device, train_loader, criterion, epochs, test_loader):
+    optimizer = optim.SGD(model.parameters(), lr=0.05,
+                                        momentum=0.9, weight_decay=0.0005)
+    scheduler = CosineAnnealingLR(optimizer, T_max=30)
     model.train()
     for epoch in range(1, epochs+1):
         runningloss = 0
@@ -37,6 +39,9 @@ def train(model, device, train_loader, criterion, epochs):
             optimizer.step()
             runningloss += loss.item()
         runningloss /= len(train_loader)
+        loss, accuracy = test(model, device, criterion, test_loader)
+        scheduler.step()
+        print(f"{epoch}, {runningloss}, {loss}, {accuracy}")
 
 def test(model, device, criterion, test_loader):
     model.eval()
@@ -46,9 +51,10 @@ def test(model, device, criterion, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += criterion(output, target)
+            test_loss += criterion(output, target).item()
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
+        
     test_loss /= len(test_loader)
     acc = correct / len(test_loader.dataset)
     return test_loss, acc
@@ -63,6 +69,8 @@ def main():
 
     train_dataset = datasets.CIFAR10(os.path.join('../data', 'cifar10'), train=True, download=True,
                                     transform=transforms.Compose([
+                                        transforms.RandomCrop(32, padding=4),
+                                        transforms.RandomHorizontalFlip(),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                                     ]))
@@ -75,21 +83,14 @@ def main():
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=64, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-        test_dataset[0], batch_size=50, shuffle=False, **kwargs)
-    print(train_dataset)
-    print(test_dataset[0])
-    print(test_dataset[1])
-    exit(1)
+        test_dataset[0], batch_size=100, shuffle=False, **kwargs)
     
-    model = Conv6()
+    model = ResNet18()
 
     model = model.to(device)
     # NOTE: only pass the parameters where p.requires_grad == True to the optimizer! Important!
     criterion = nn.CrossEntropyLoss()
-    for epoch in range(250):
-        train(model, device, train_loader, criterion, 1)
-        loss, accuracy = test(model, device, criterion, test_loader)
-        print(f"{epoch}, {loss}, {accuracy}")
+    train(model, device, train_loader, criterion, 30, test_loader)
 
 if __name__ == '__main__':
     main()
